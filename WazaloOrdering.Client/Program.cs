7e08870bdf3357c5896ee8f5373b2094709603ff
Per Ivar Nerseth <perivar@nerseth.com>
2018-05-25 20:15:57 +0200

    Fixed styling and added clipboard.js

--- WazaloOrdering.Client/Program.cs
+++ WazaloOrdering.Client/Program.cs
@@ -18,9 +18,24 @@ namespace WazaloOrdering.Client
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
+
+            /*
+            The tasks that are performed by CreateDefaultBuilder is
+            1. Configures Kestrel as the web server.
+            2. Sets the content root to Directory.GetCurrentDirectory.
+            3. Loads optional configuration from
+                a) Appsettings.json
+                b) Appsettings.{Environment}.json.
+                c) User secrets when the app runs in the Development environment.
+                d) Environment variables
+                e) Command-line arguments.
+            4. Enable logging
+            5. Integrates the Kestrel run with IIS             
+             */             
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAppConfiguration(builder =>
                 {
+                    // Add UserSecrets fir both development and Production environment
                     builder.AddUserSecrets<Startup>();
                 })
                 .UseStartup<Startup>()
